3f967cf434409dfc895dc0ee26a121ecfd5e7653
Piyush1610 <143530721+Piyush16102005@users.noreply.github.com>
2026-04-03 09:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ scaler = joblib.load("scaler.pkl")
 features = [
     'age', 'anaemia', 'creatinine_phosphokinase', 'diabetes',
     'ejection_fraction', 'high_blood_pressure', 'platelets',
-    'serum_creatinine', 'serum_sodium', 'sex', 'smoking',
+    'serum_creatinine', 'serum_sodium', 'sex', 'smoking' 'time',
 ]
 
 @app.route('/')
